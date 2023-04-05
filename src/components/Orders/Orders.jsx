e87ad4e61d1f5cc300ae14ec7e92fb901650bfd2
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-04-05 02:45:48 -0700

    implemented delete from cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Order.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart =useLoaderData();
-    console.log(cart)
+    const saveCart =useLoaderData();
+
+    const [cart,setCart] =useState(saveCart);
+    // console.log(saveCart)
+    const handelRemoveFromCart = (id)=>{
+        const remaining =cart.filter(product => product.id !==id);
+        setCart(remaining)
+        removeFromDb(id)
+    }
+
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-            <h2>Orders page:{cart.length}</h2>
+            <div className='review-container'>
+                {
+                    cart.map(product =><ReviewItem
+                    key={product.id}
+                    product={product}
+                    handelRemoveFromCart={handelRemoveFromCart}
+                    ></ReviewItem>)
+                }
             </div>
             <dvi className='cart-container'>
                 <Cart cart={cart}></Cart>
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const ReviewItem = ({ product,handelRemoveFromCart }) => {
+    // console.log(product)
+    const { name, img, quantity, price, id } = product;
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button onClick={()=>handelRemoveFromCart(id)} className='btn-delate'>
+                <FontAwesomeIcon className='delate-icon' icon={faTrashAlt} />
+            </button>
+        </div>
+    );
+};
+
+export default ReviewItem;
